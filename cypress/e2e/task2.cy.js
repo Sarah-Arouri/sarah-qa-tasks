3c80a678f3aa2c1221553296434bd73197c84736
Arouri <sarah.arouri.2018.com>
2023-11-11 23:36:00 +0200

    Solution for task2

---
+++ cypress/e2e/task2.cy.js
@@ -0,0 +1,59 @@
+it("task",()=>{ 
+    cy.visit("/checkout/#shipping"); 
+    /* ==== Generated with Cypress Studio ==== */ cy.get('#search').clear('s');
+    cy.get('#search').type('shirt{enter}');
+    cy.get(':nth-child(2) > .product-item-info > .photo > .product-image-container > .product-image-wrapper > .product-image-photo').click(); 
+    cy.get('#option-label-size-143-item-167').click(); 
+    cy.get('.swatch-attribute.color > .swatch-attribute-options').click();
+    cy.get('#option-label-color-93-item-57').click();
+    cy.get('#product-addtocart-button > span').click(); 
+    cy.wait(5000); 
+    cy.get('.showcart').click(); 
+    cy.get('#top-cart-btn-checkout').click(); 
+    cy.wait(7000); /* ==== End Cypress Studio ==== */
+     //  start writing your code from here })
+    // cy.get("shippingAddress.firstname");
+    // cy.get("button action continue primary")
+    //cy.get('#shipping-new-address-form [name="firstname"]');
+
+    //for Email Address
+    cy.get("#customer-email-fieldset #customer-email");
+    //.field.required [name="username"]
+    //for first name
+    cy.get(".control [name='firstname']");
+    //for last name
+    cy.get(".control [name='lastname']");
+    //for company 
+    cy.get(".control [name='company']");
+    //for street name 
+    //field1
+    cy.get("[name='street[0]']");
+    //feild2
+    cy.get("[name='street[1]']");
+    //feild3
+    cy.get("[name='street[2]']");
+    //for city name
+    cy.get("[name='city']");
+    //for state
+    cy.get("[name='region_id']");
+    //for postal code
+    cy.get("[name='postcode']");
+    //for country
+    cy.get("[name='country_id']");
+    //for phone number
+    cy.get("[name='telephone']");
+    //in the shipping method 
+    //the first row
+    cy.get("[value='tablerate_bestway']");
+    //the second row
+    cy.get("[value='flatrate_flatrate']");
+    //for the button 
+     cy.get(".primary .button");
+
+    
+    // cy.get("#VW9EQOG");
+    // cy.get("#customer-email-fieldset");
+    // cy.get(".field.street");
+    // cy.get([name="shippingAddress.city"])
+})
+
