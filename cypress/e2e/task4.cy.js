b5bfcc374eb837855d423eb1c7570b437022c3ba
Sarah Arouri <sarah.arouri.2018@gmail.com>
2023-11-29 18:18:45 +0200

    Solution for task4

--- cypress/e2e/task4.cy.js
+++ cypress/e2e/task4.cy.js
@@ -5,9 +5,11 @@ describe("task4",()=>{
         //cy.contains("a","Radiant Tee").click();
         //cy.wait("6000");
         cy.get("[class='product-image-container']:eq(0)").click();
+        cy.wait(5000);
+        //cy.get("[option-tooltip-value='M']").click();
         cy.get("[option-label='M']").click();
-        cy.get("[option-label='Purple']").click();
-        cy.get("[id='qty']").clear().type(3);
+        //cy.get("[option-label='Purple']").click();
+        cy.get(".swatch-attribute-options .color:last-child").click();
         cy.get("[id='product-addtocart-button']").click();
         
     })
