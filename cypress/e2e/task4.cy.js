0ee21a235da23ec3c2a8fad0f5dc591ed4af791b
Sarah Arouri <sarah.arouri.2018@gmail.com>
2023-11-29 17:54:20 +0200

    Solution for task4

--- cypress/e2e/task4.cy.js
+++ cypress/e2e/task4.cy.js
@@ -2,9 +2,10 @@ describe("task4",()=>{
     it("tests",()=>{
         cy.visit("https://magento.softwaretestingboard.com");
         cy.get("#search").type("Shirt{enter}");
-        cy.contains("a","Radiant Tee").click();
+        //cy.contains("a","Radiant Tee").click();
         //cy.wait("6000");
-        cy.get("[id='option-label-size-143-item-168']").click();
+        cy.get("[class='product-image-container']:eq(0)").click();
+        cy.get("[option-label='M']").click();
         cy.get("[option-label='Purple']").click();
         cy.get("[id='qty']").clear().type(3);
         cy.get("[id='product-addtocart-button']").click();
