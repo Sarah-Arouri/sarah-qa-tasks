9bef148a5a3fc6a3b1a6899f5b80bda6499e7de2
Sarah Arouri <sarah.arouri.2018@gmail.com>
2023-12-02 19:10:59 +0200

    Solution for task5

--- cypress/e2e/task5.cy.js
+++ cypress/e2e/task5.cy.js
@@ -2,33 +2,26 @@
 describe("Creating an account",()=>{
     it("Create Account Field",()=>{
         cy.visit("https://magento.softwaretestingboard.com/customer/account/create/");
-        //cy.get(".logo").should("be.visible");
-        //cy.get("#search").should("be.visible");
-        //cy.get(".showcart").should("be.visible");
-        //cy.get(".counter").first().should("be.hidden");
-        //cy.get(".navigation").should("be.visible");
-        //cy.get(".base").should("have.text","Create New Customer Account");
-        //cy.get(".legend").first().should("have.text","Personal Information");
-        cy.get("#firstname").should("be.empty").and("be.visible");
-        cy.get("#lastname").should("be.empty").and("be.visible");
-        //cy.get(".legend").last().should("have.text","Sign-in Information").and("be.visible");
-        cy.get("#email_address").should("be.empty").and("be.visible");
-        cy.get("#password").should("be.empty").and("be.visible");
-        cy.get("[name='password_confirmation']").should("be.empty").and("be.visible");
+        cy.get("#firstname").should("be.visible");
+        cy.get("#lastname").should("be.visible");
+        cy.get("#email_address").should("be.visible");
+        cy.get("#password").should("be.visible");
+        cy.get("[name='password_confirmation']").should("be.visible");
         cy.get("[class='action submit primary']").should("be.visible").and("be.enabled");
      })
 
-     it("Happy Scenario",()=>{
-        cy.visit("https://magento.softwaretestingboard.com/customer/account/create/");
-        cy.get("#firstname").type("ahmad{enter}");
-        cy.get("#lastname").type("hajja{enter}");
-        cy.get("#email_address").type("hdhuj7@gmail.com{enter}");
-        cy.get("#password").type("ahmad!123{enter}");
-        cy.get("[name='password_confirmation']").type("ahmad!123{enter}");
-        cy.get("[class='action submit primary']").click();
-        cy.get("[class='messages']").should("be.visible")
+     it.only("Happy Scenario",()=>{
+      cy.visit("https://magento.softwaretestingboard.com/customer/account/create/");
+      cy.get("#firstname").type("hd4d1wpw1iff");
+      cy.get("#lastname").type("bdk411www1fff");
+      cy.get("#email_address").type(" h11j1dd4wwwu12@gmail.com");
+      cy.get("#password").type("jojo!123");
+      cy.get("[name='password_confirmation']").type("jojo!123");
+      cy.get("[class='action submit primary']").click({ multiple: true });
+      cy.wait(2000);
+      cy.get(".messages [data-bind]").last().should("have.text","Thank you for registering with Main Website Store.");
+       })
         
 
 
      })
-})
